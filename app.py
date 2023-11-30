c2541fbee478bb051bbcd4d4d6f77cbe7836d476
kpomroy <97473278+kpomroy@users.noreply.github.com>
2023-11-30 17:27:34 -0500

    Allowing for new users to be added to database

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, g
 import traceback
 import csv
-from password_crack import authenticate
+from password_crack import authenticate, hash_pw
+from database import add_user, get_password
 
 app = Flask(__name__)
+
+PASSWORD_MIN_LENGTH = 8
+PASSWORD_MAX_LENGTH = 25
+SPECIAL_CHAR = "!@#$%^&*"
 
 loginAttempts = 1
 
@@ -34,9 +39,44 @@ def getEmployeeInfo():
 # Function to validate user credentials
 def login(username, password):
     if username in employeeLogins:
-        storedPassword = employeeLogins[username]
+        storedPassword = get_password(username)
         return authenticate(storedPassword, password)
     return False
+
+def password_strength(test_password) -> bool:
+    """
+    Check basic password strength. Return true if password
+    meets minimum complexity criteria, false otherwise.
+
+    :param test_password: str
+    :return: bool
+    """
+    if test_password.isalnum() or test_password.isalpha():
+        return False
+    if len(test_password) < PASSWORD_MIN_LENGTH:
+        return False
+    if len(test_password) > PASSWORD_MAX_LENGTH:
+        return False
+    special_char_check = False
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    for ch in test_password:
+        if ch in SPECIAL_CHAR:
+            special_char_check = True
+        if ch.isupper():
+            has_upper = True
+        if ch.islower():
+            has_lower = True
+        if ch.isdigit():
+            has_digit = True
+    if not special_char_check or \
+            not has_upper or \
+            not has_lower or \
+            not has_digit:
+        return False
+    else:
+        return True
 
 # Function to verify if an employee has access to the menu option they chose
 # admin: full access
@@ -81,6 +121,25 @@ def login_page():
     else:
         return render_template('login.html', error=None)
 
+# Flask route for new user page
+@app.route('/new_user', methods= ['GET', 'POST'])
+def create_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # check if username is available
+        if username not in employeeAccess:
+            # validate password
+            if password_strength(password):
+                hashedPassword = hash_pw(password)
+                add_user(username, hashedPassword)
+                return render_template('login.html', error='User sucessfully created')
+            else:
+                return render_template('new_user.html', error = 'Password not complex enough')
+        else:
+            return render_template('new_user.html', error='Username not available')
+    return render_template('new_user.html')
+
 # Flask route for the menu page
 @app.route('/menu/<username>')
 def menu(username):
@@ -92,13 +151,13 @@ def menu(username):
 def menu_option(username, choice):
     if checkEmployeeAccess(username, choice):
         if choice == 1:
-            return f'You accessed the Time Reporting area, {username}!'
+            return f'You accessed the Time Reporting area'
         elif choice == 2:
-            return f'You accessed the Accounting area, {username}!'
+            return f'You accessed the Accounting area'
         elif choice == 3:
-            return f'You accessed the IT Helpdesk area, {username}!'
+            return f'You accessed the IT Helpdesk area'
         elif choice == 4:
-            return f'You accessed the Engineering Documents area, {username}!'
+            return f'You accessed the Engineering Documents area!'
         else:
             return 'Exiting the system'
     else:
